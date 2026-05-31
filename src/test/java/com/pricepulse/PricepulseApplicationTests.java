9226e16f09d041b5364a1347f4c4d47c1524da05
JavacAman <er.amankumar310@gmail.com>
2026-05-31 17:57:42 +0530

    testcase fix

--- src/test/java/com/pricepulse/PricepulseApplicationTests.java
+++ src/test/java/com/pricepulse/PricepulseApplicationTests.java
@@ -2,13 +2,13 @@ package com.pricepulse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class PricepulseApplicationTests {
 
-	@MockBean
+	@MockitoBean
 	JavaMailSender mailSender;
 
 	@Test
